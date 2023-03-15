3baad9d3144d38d96e7e1bafb3b8933a39d69c15
yphill <phelipequeiroz2004@gmail.com>
2023-03-14 21:38:55 -0300

    adaptador, construtor e view holder

--- app/src/main/java/br/com/etecia/recycleviewfilmes/Adaptador.java
+++ app/src/main/java/br/com/etecia/recycleviewfilmes/Adaptador.java
@@ -2,8 +2,11 @@ package br.com.etecia.recycleviewfilmes;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -29,8 +32,17 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
     //criando classe viewholder
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView idTituloFilme;
+        ImageView idImgFIlmes;
+        CardView idCardFilmes;
+
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTituloFilme = itemView.findViewById(R.id.idTituloFilme);
+            idImgFIlmes = itemView.findViewById(R.id.idImgFilmes);
+            idCardFilmes = itemView.findViewById(R.id.idCardFilmes);
         }
     }
 
